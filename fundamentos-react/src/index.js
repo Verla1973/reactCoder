637c1fe0b8d387975b97bbeb84b1b8150c140f44
Eskurinho Dev <manoads2019@gmail.com>
2020-10-02 18:07:43 -0300

    react

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import ReactDom from 'react-dom'
+import React from 'react'
+
+ReactDom.render(
+    <div>
+        <strong>Olá Eskurinho</strong>
+    </div>,
+    document.getElementById('root')
+)
