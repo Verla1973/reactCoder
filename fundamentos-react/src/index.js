99d24630eadaa6268255597460678bc69ead169a
Eskurinho Dev <manoads2019@gmail.com>
2020-10-03 21:19:11 -0300

    commit componentes com parametros

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,10 +3,14 @@ import ReactDom from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDom.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
+        <ComParametro titulo="Situação do Aluno" 
+        aluno = "Eskurinho Dev "
+        nota = {9.7} />
     </div>,
     document.getElementById('root')
 )
